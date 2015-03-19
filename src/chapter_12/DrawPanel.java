612cadb8664197df3afb8ab6b0b2361d7a8c524c
dskrypa <dskrypa@gmail.com>
2015-03-19 16:25:17 -0400

    edit

--- src/chapter_12/DrawPanel.java
+++ src/chapter_12/DrawPanel.java
@@ -25,26 +25,6 @@ Class DrawPanel should also provide event handling to enable the user to draw wi
 The constructor should then set the background color of the DrawPanel to Color.WHITE and
 register  the  MouseListener and  MouseMotionListener so  the  JPanel properly handles mouse events.
 
-Next, create a JFrame subclass called DrawFrame that provides a GUI that enables the user to
-control various aspects of drawing.
-
-For the layout of the DrawFrame, we recommend a BorderLayout, with the components in the NORTH region,
-the main drawing panel in the CENTER region, and a status bar in the SOUTH region, as in Fig. 12.49.
-
-In the top panel, create the components listed below.
-Each component’s event handler should call the appropriate method in class DrawPanel.
-
-a) A button to undo the last shape drawn.
-b) A button to clear all shapes from the drawing.
-c) A combo box for selecting the color from the 13 predefined colors.
-d) A combo box for selecting the shape to draw.
-e) A checkbox that specifies whether a shape should be filled or unfilled. 
-
-Declare and create the interface components in DrawFrame’s constructor.
-You’ll need to create the status bar JLabel before you create the DrawPanel, so you
-can pass the JLabel as an argument to DrawPanel’s constructor.
-
-Finally, create a test class that initializes and displays the DrawFrame to execute the application.
 */
 
 
